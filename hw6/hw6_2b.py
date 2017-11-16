aa8b347c2a0084447a726d87f58c8d6f768dcc15
Paul Boone <narcissus.pong@gmail.com>
2017-11-15 19:34:20 -0500

    labling, general conversion

--- hw6/hw6_2b.py
+++ hw6/hw6_2b.py
@@ -8,16 +8,22 @@ def reactor_func(concs, t):
         Define the right-hand side of equation dy/dt = a*y
     """
 
-    k1 = 4.64e-4 # 1/(M*s)
-    r1 = -k1 * concs[0] * concs[1]
+    k1 = 3.23e-3 # 1/(M*s)
+    temp = 313.15 # kelvins
+    r_const = 8.314
+    ka = 3 # L / mole
+    kb = 2 # L / mole
+    pa = concs[0] # mole / L
+    pb = concs[1] # mole / L
 
+    r1 = -k1 * ka*pa*kb*pb / (1 + ka*pa + kb*pb)**2
     return np.array([r1, r1, -r1, -r1])
 
 
 # Initial concentrations
 y0 = [1.0, 1.1, 0, 0]
 
-t_end = 80000
+t_end = 10000
 num_points = 100
 t = np.linspace(0, t_end, num_points)
 
@@ -35,12 +41,12 @@ ax.grid(linestyle='-', color='0.7', zorder=0)
 ax.set_xlabel('t [seconds]')
 ax.set_ylabel('concentration [M]')
 ax.legend(["C_a","C_b", "C_c", "C_d", "C_e", "C_p"])
-conversion_limit = [i for i,yval in enumerate(y) if yval[0] < 0.05*y0[0]]
+conversion_85 = [i for i,yval in enumerate(y) if yval[0] < 0.05*y0[0]]
 subheader = ""
-if conversion_limit:
-    time_at_conversion_limit = conversion_limit[0] * (t_end / num_points)
-    subheader = "\nTime at 95%% conversion:  %s seconds" % time_at_conversion_limit
-    ax.axvspan(0,time_at_conversion_limit, color='0.9')
+if conversion_85:
+    time_at_conversion_85 = conversion_85[0] * (t_end / num_points)
+    subheader = "\nTime at 95%% conversion:  %s seconds" % time_at_conversion_85
+    ax.axvspan(0,time_at_conversion_85, color='0.9')
 
-ax.set_title("Concentration vs Time (w/o catalyst)" + subheader)
+ax.set_title("Concentration vs Time with Catalyst" + subheader)
 fig.savefig("conc-vs-time.png", dpi=144)
