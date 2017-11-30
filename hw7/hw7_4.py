d0f68edbe45761b7c432effc56d7f18c3c8f9430
Paul Boone <narcissus.pong@gmail.com>
2017-11-29 20:32:48 -0500

    actually plausible results

--- hw7/hw7_4.py
+++ hw7/hw7_4.py
@@ -16,11 +16,11 @@ const_r = 0.082057 # L atm K−1 mol−1
 const_r_J = 8.314 # J / mol K
 c_h = 0.01692573879495781 / 2.0
 
-k_f = 1e-5
-k_b = 1e-7
+k_f = 1e1
+k_b = 1e-1
 
 dG = -76200 # J / mol
-e_a_diff = dG + const_r_J * 830.1 * math.log(k_f / k_b)
+e_a_diff = dG + const_r_J * 298.15* math.log(k_f / k_b)
 e_a_diff
 e_a_f = 10000 # J / mol
 e_a_b = e_a_f - e_a_diff
@@ -30,20 +30,19 @@ desired_conversion = 0.1
 
 temps = np.linspace(360, 1000, 641)
 y = np.exp(-(-118780 / (const_r_J * temps) + 142.94 / const_r_J))
-np.isfinite(temps)
 
-
-
-fig1 = plt.figure(figsize=(7,7))
-ax = fig1.add_subplot(1, 1, 1)
-ax.plot(temps, y, zorder=2)
-ax.set_xlabel('temp [K]')
-ax.set_ylabel('K_eq')
-ax.grid(linestyle='-', color='0.7', zorder=0)
-ax.set_ylim(0.1,100)
-ax.set_yscale('log')
-
-# fig.savefig("conc-vs-time.png", dpi=288)
+# fig1 = plt.figure(figsize=(7,7))
+# ax = fig1.add_subplot(1, 1, 1)
+# ax.plot(temps, y, zorder=2)
+# # ax.plot(temps, k1, zorder=2)
+# # ax.plot(temps, k2, zorder=2)
+# ax.set_xlabel('temp [K]')
+# ax.set_ylabel('K_eq')
+# ax.grid(linestyle='-', color='0.7', zorder=0)
+# ax.set_ylim(0.1,1e9)
+# ax.set_yscale('log')
+#
+# fig1.savefig("Keq graph.png", dpi=288)
 
 
 t_opt = (e_a_b - e_a_f)  / (const_r_J * math.log(((e_a_b * k_b) / (e_a_f * k_f)) * (desired_conversion / (1 - desired_conversion))))
@@ -57,8 +56,11 @@ def reactor_func(vars, t):
     f0, f1, f2, temp, temp_hx = vars
 
 
-    k1 = k_f * np.exp(-(-118780 / (const_r_J * temp) + 142.94 / const_r_J))
-    k2 = k_b * np.exp(-(118780 / (const_r_J * temp) - 142.94 / const_r_J))
+    k1 = k_f * np.exp(-e_a_f / (const_r_J * temp))
+    k2 = k_b * np.exp(-e_a_b / (const_r_J * temp))
+
+    if (t == 0.0):
+        print(t, k1,k2)
 
     total_flow = f0 + f1 + f2  # mol / s
     ig_volumetric_flow = pressure / (const_r * temp) # mol / L
@@ -69,9 +71,9 @@ def reactor_func(vars, t):
     return np.array([-r1, -r1, r1, dTdV, dTedVs])
 
 # Initial concentrations
-f0 = [c_h, c_h, 0, 460.00, 463.15]
+f0 = [c_h, c_h, 0, 930.00, 463.15]
 
-t_end = 20
+t_end = 10
 num_points = 100
 t = np.linspace(0, t_end, num_points)
 
@@ -99,15 +101,15 @@ ax2.set_ylim(300, 900)
 ax2.set_ylabel('Temp')
 ax2.legend(loc='right')
 
-conversion_85 = [i for i,yval in enumerate(y) if yval[0] < 0.15*f0[0]]
+conversion_xx = [i for i,yval in enumerate(y) if yval[0] < (1 - desired_conversion) * f0[0]]
 subheader = ""
 temp_subheader = ""
-if conversion_85:
-    volume_at_conversion_85 = conversion_85[0] * (t_end / num_points)
-    temp_at_conversion_85 = y[conversion_85[0],3]
-    subheader = "\nVolume at 85%% conversion:  %2.1f L" % volume_at_conversion_85
-    temp_subheader = "\nTemp at 85%% conversion:  %2.1f K" % temp_at_conversion_85
-    ax.axvspan(0,volume_at_conversion_85, color='0.9')
+if conversion_xx:
+    volume_at_conversion_xx = conversion_xx[0] * (t_end / num_points)
+    temp_at_conversion_xx = y[conversion_xx[0],3]
+    subheader = "\nVolume at %2.0f%% conversion:  %2.1f L" % (desired_conversion * 100, volume_at_conversion_xx)
+    temp_subheader = "\nTemp at %2.0f%% conversion:  %2.1f K" % (desired_conversion * 100, temp_at_conversion_xx)
+    ax.axvspan(0,volume_at_conversion_xx, color='0.9')
 
 ax.set_title("Flowrate vs Cumulative Volume (PFR)" + subheader)
 
@@ -124,4 +126,4 @@ ax.set_title("Flowrate vs Cumulative Volume (PFR)" + subheader)
 # temp_subheader = "\nUA/V = %2.1f" % ua_v
 # ax.set_title('Temp vs Cumulative Volume (PFR)' + temp_subheader)
 # ax.axhspan(450,673.15, color='0.9')
-# fig.savefig("pfr-flow-vs-vol.png", dpi=288)
+fig.savefig("pfr-flow-vs-vol.png", dpi=288)
